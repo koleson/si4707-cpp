43a251279ec59413ae2bfe51fde821558316c425
Kiel Oleson <kiel@kmo.email>
2023-10-10 22:56:32 -0700

    speculative fix for comments / = in defines

--- si4707_const.h
+++ si4707_const.h
@@ -32,19 +32,52 @@
 
 // Si4707 Properties
 
-#define SI4707_PROP_GPO_IEN =                    0x0001			// defaults to 0x0000
-#define SI4707_PROP_REFCLK_FREQ =                0x0201			// defaults to 0x8000
-#define SI4707_PROP_REFCLK_PRESCALE =            0x0202			// defaults to 0x0001
-#define SI4707_PROP_RX_VOLUME =                  0x4000			// defaults to 0x003f
-#define SI4707_PROP_RX_HARD_MUTE =               0x4001			// defaults to 0x0000
-#define SI4707_PROP_WB_MAX_TUNE_ERROR =          0x5108			// defaults to 0x000a
-#define SI4707_PROP_WB_RSQ_INT_SOURCE =          0x5200			// defaults to 0x0000
-#define SI4707_PROP_WB_RSQ_SNR_HI_THRESH =       0x5201			// defaults to 0x007f
-#define SI4707_PROP_WB_RSQ_SNR_LO_THRESH =       0x5202			// defaults to 0x0000
-#define SI4707_PROP_WB_RSQ_RSSI_HI_THRESH =      0x5203			// defaults to 0x007f
-#define SI4707_PROP_WB_RSQ_RSSI_LO_THRESH =      0x5204			// defaults to 0x0000
-#define SI4707_PROP_WB_VALID_SNR_THRESH =        0x5403			// defaults to 0x0003
-#define SI4707_PROP_WB_VALID_RSSI_THRESH =       0x5404			// defaults to 0x0014
-#define SI4707_PROP_WB_SAME_INTERRUPT_SOURCE =   0x5500			// defaults to 0x0000
+// defaults to 0x0000
+#define SI4707_PROP_GPO_IEN 					0x0001
+
+// defaults to 0x8000
+#define SI4707_PROP_REFCLK_FREQ 				0x0201
+
+// defaults to 0x0001
+#define SI4707_PROP_REFCLK_PRESCALE				0x0202
+
+// defaults to 0x003f
+#define SI4707_PROP_RX_VOLUME					0x4000
+
+// defaults to 0x0000
+#define SI4707_PROP_RX_HARD_MUTE				0x4001
+
+// defaults to 0x000a
+#define SI4707_PROP_WB_MAX_TUNE_ERROR			0x5108
+
+// defaults to 0x0000
+#define SI4707_PROP_WB_RSQ_INT_SOURCE			0x5200
+
+// defaults to 0x007f
+#define SI4707_PROP_WB_RSQ_SNR_HI_THRESH		0x5201
+
+// defaults to 0x0000
+#define SI4707_PROP_WB_RSQ_SNR_LO_THRESH		0x5202
+
+// defaults to 0x007f
+#define SI4707_PROP_WB_RSQ_RSSI_HI_THRESH		0x5203
+
+// defaults to 0x0000
+#define SI4707_PROP_WB_RSQ_RSSI_LO_THRESH		0x5204
+
+// defaults to 0x0003
+#define SI4707_PROP_WB_VALID_SNR_THRESH			0x5403
+
+// defaults to 0x0014
+#define SI4707_PROP_WB_VALID_RSSI_THRESH		0x5404
+
+// defaults to 0x0000
+#define SI4707_PROP_WB_SAME_INTERRUPT_SOURCE	0x5500
+
+// Si4707 SAME machine states
+#define SI4707_SAME_STATE_END_OF_MESSAGE 		0
+#define SI4707_SAME_STATE_PREAMBLE_DETECTED 	1
+#define SI4707_SAME_STATE_RECEIVING_HEADER		2
+#define SI4707_SAME_STATE_HEADER_COMPLETE		3
 
 // TODO:  this list of available properties is probably not complete.  check AN332.  kmo 9 oct 2023
