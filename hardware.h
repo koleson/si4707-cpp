a99fe78ee552cbaa4f142ed2479f00717d612166
Kiel Oleson <kiel@kmo.email>
2023-10-10 21:18:22 -0700

    more hardware config / aliases

--- hardware.h
+++ hardware.h
@@ -2,20 +2,25 @@
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 // NB:  these are default pins and the pins used by the Wiznet W5500 pico.  kmo 9 oct 2023 17h42
-#define SPI_PORT spi0
+#define SPI_PORT spi_default
 #define PIN_MISO 16
 #define PIN_CS   17
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
 // I2C defines - not defaults, specific to Wiznet W5500 setup
-#define I2C_PORT i2c1
-#define I2C_SDA 26
-#define I2C_SCL 27
+// pins 26/27 are on i2c1.
+#define I2C_PORT i2c_default
+#define I2C_SDA 4
+#define I2C_SCL 5
 
 
 // Si4707 defines
 #define SI4707_RESET 28
+#define SI4707_INT 22
+#define SI4707_SEN 17
+#define SI4707_GPO1 16
+#define SI4707_GPO2 22
 
 // default 0x63, can be set to 0x11 by pulling SEN low during reset
 #define SI4707_ADDR 0x63
