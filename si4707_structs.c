d395ea605492aad41cf81183551da82fbd068961
Kiel Oleson <kiel@jusuqici.org>
2023-12-04 11:24:47 -0800

    extract Si4707 structs to separate header; add compare

---
+++ si4707_structs.c
@@ -0,0 +1,22 @@
+//
+// Created by koleson on 12/4/23.
+//
+
+#include "si4707_structs.h"
+
+
+// compares only SAME status parameters of SAME status packets.
+// returns true if all status parameters of both packets are the same; false otherwise
+// does not compare received DATA or CONF (confidence).
+bool equal_SAME_status_packets(struct Si4707_SAME_Status_Packet * p1, struct Si4707_SAME_Status_Packet * p2) {
+    if (
+        p1->EOMDET == p2->EOMDET
+        && p1->SOMDET == p2->SOMDET
+        && p1->PREDET == p2->PREDET
+        && p1->HDRRDY == p2->HDRRDY
+        && p1->STATE == p2->STATE
+        && p1->MSGLEN == p1->MSGLEN
+            ) { return true; }
+    return false;
+}
+
