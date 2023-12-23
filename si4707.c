c622cd3c37614d3cecffd9c17235837769b85b64
Kiel Oleson <kiel@jusuqici.org>
2023-12-23 12:58:57 -0800

    si4707:  use caller-provided INTACK value

--- si4707.c
+++ si4707.c
@@ -385,7 +385,8 @@ void get_si4707_same_status(const struct Si4707_SAME_Status_Params *params, stru
 
 	struct Si4707_SAME_Status_Params same_buf_params;
 	struct Si4707_SAME_Status_Packet same_buf_packet;
-	same_buf_params.INTACK = 0;
+	printf("si4707.c: get_si4707_same_status: INTACK = %u", params->INTACK);
+	same_buf_params.INTACK = params->INTACK;
 	same_buf_params.READADDR = 0;
 
 
